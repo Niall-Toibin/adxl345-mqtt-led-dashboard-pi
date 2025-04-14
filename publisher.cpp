7742ab40c0f74ab71665690aa2f804d745b45c2a
Niall Toibin <niall.toibin2@mail.dcu.ie>
2025-04-14 19:48:54 +0100

    Added looping functionality to publisher. Checks readings every second

--- publisher.cpp
+++ publisher.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <string>
 #include <cstring>
+#include <unistd.h>
 #include "MQTTClient.h"
 #include "ADXL345.h"
 #include "I2CDevice.h"
@@ -11,7 +12,7 @@
 using namespace std;
 using namespace exploringRPi;
 
-#define ADDRESS "tcp://192.168.1.121:1883"
+#define ADDRESS "tcp://192.168.1.124:1883"
 #define CLIENTID "rpi1"
 #define AUTHMETHOD "niall"
 #define AUTHTOKEN "tkno2mmg"
@@ -39,7 +40,6 @@ string getLocalTime() {
 }
 
 int main(int argc, char* argv[]) {
-    char str_payload[256];
     MQTTClient client;
     MQTTClient_connectOptions opts = MQTTClient_connectOptions_initializer;
     MQTTClient_message pubmsg = MQTTClient_message_initializer;
@@ -68,38 +68,43 @@ int main(int argc, char* argv[]) {
     ADXL345 sensor(1, 0x53);
     sensor.setRange(ADXL345::PLUSMINUS_4_G);
     sensor.setResolution(ADXL345::NORMAL);
-    sensor.readSensorState();
-    float pitch = sensor.getPitch();
-    float roll  = sensor.getRoll();
-    float temp = getCPUTemperature();
-    string time = getLocalTime();
-    
-    snprintf(str_payload, sizeof(str_payload),
-             "{\"d\":{\"pitch\":%.2f,\"roll\":%.2f,\"tempC\":%.2f,\"time\":\"%s\"}}",
-             pitch, roll, temp, time.c_str());
-    
-    pubmsg.payload = str_payload;
-    pubmsg.payloadlen = (int)strlen(str_payload);
-    pubmsg.qos = QOS;
-    pubmsg.retained = 0;
-    
-    MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
-    cout << "Waiting for up to " << (int)(TIMEOUT/1000)
-         << " seconds for publication of " << str_payload
-         << " on topic " << TOPIC << " for ClientID: " << CLIENTID << endl;
-    rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
+    while (true) {
+        sensor.readSensorState();
+        float pitch = sensor.getPitch();
+        float roll  = sensor.getRoll();
+        float temp  = getCPUTemperature();
+        string timeStr = getLocalTime();
 
-    // Publishes just the pitch to topic een1071/pitch
-    char pitch_payload[256];
-    snprintf(pitch_payload, sizeof(pitch_payload),
-             "{\"d\":{\"pitch\":%.2f}}", pitch);
+        char str_payload[256];
+        snprintf(str_payload, sizeof(str_payload),
+            "{\"d\":{\"pitch\":%.2f,\"roll\":%.2f,\"tempC\":%.2f,\"time\":\"%s\"}}",
+            pitch, roll, temp, timeStr.c_str());
 
-    pubmsg.payload = pitch_payload;
-    pubmsg.payloadlen = (int)strlen(pitch_payload);
-    // same QoS, etc.
-    MQTTClient_publishMessage(client, PITCH_TOPIC, &pubmsg, &token);
-    cout << "Publishing pitch-only to " << PITCH_TOPIC << endl;
-    rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
+        pubmsg.payload = str_payload;
+        pubmsg.payloadlen = (int)strlen(str_payload);
+        pubmsg.qos = QOS;
+        pubmsg.retained = 0;
+
+        MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+        cout << "Published: " << str_payload 
+             << " to topic " << TOPIC << endl;
+        rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
+
+        // Also publish pitch to een1071/pitch
+        char pitch_payload[256];
+        snprintf(pitch_payload, sizeof(pitch_payload),
+                "{\"d\":{\"pitch\":%.2f}}", pitch);
+
+        pubmsg.payload = pitch_payload;
+        pubmsg.payloadlen = (int)strlen(pitch_payload);
+
+        MQTTClient_publishMessage(client, PITCH_TOPIC, &pubmsg, &token);
+        cout << "Published pitch-only to " << PITCH_TOPIC << endl;
+        rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
+
+        sleep(1);
+    }
+
     MQTTClient_disconnect(client, 10000);
     MQTTClient_destroy(&client);
     return rc;
